b86bcd0d84462bafc66e8372ec7ee2bd604ebb74
felipy roma <felipy.roma@hotmail.com>
2022-07-08 02:57:04 -0300

    att

---
+++ js/calculator.js
@@ -0,0 +1,14 @@
+const resultado = document.getElementById('res')
+
+function add(valor) {
+  resultado.innerHTML += valor
+}
+
+
+function clean() {
+    resultado.innerHTML = ''
+}
+
+
+
+// function verificar()
