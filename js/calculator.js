190dad42797ae481381c78b901358166705672da
felipy roma <felipy.roma@hotmail.com>
2022-07-12 03:02:49 -0300

    att

--- js/calculator.js
+++ js/calculator.js
@@ -1,14 +1,32 @@
-const resultado = document.getElementById('res')
+const painel = document.getElementById('res')
+let equacao = ''
 
 function add(valor) {
-  resultado.innerHTML += valor
+  equacao = painel.innerHTML += valor
+  escreverNoPainel(equacao)
 }
 
+function calcular() {
+  if (equacao) {
+    escreverNoPainel(eval(equacao))
+  } else {
+    escreverNoPainel('[ERROR]')
+  }
+  
+}
+
+function cleanLastDigit() {
+  escreverNoPainel(painel.innerHTML.slice(0, -1))
+}
 
 function clean() {
-    resultado.innerHTML = ''
+  escreverNoPainel('')
+  equacao = ''
 }
 
 
+function escreverNoPainel(valor) {
+  painel.innerHTML = valor
+}
 
-// function verificar()
+
